371ce0e2684735120a25e0a67dd1cb477b54c323
Vishchunova <92801027+Vishchunova@users.noreply.github.com>
2022-05-20 00:42:41 +0300

    Create script6.js

---
+++ задание 12/script6.js
@@ -0,0 +1,22 @@
+var getData = function (keys, values) {
+  var newArray = [];
+
+  for (var i = 0; i <= values.length - 1; i++) {
+
+    var newObject = {};
+    
+    if (values[i].length - 1 <= keys.length - 1) {
+      var length = values[i].length - 1;
+    } else {
+      length = keys.length - 1
+    }
+
+    for (var j = 0; j <= length; j++) {
+      newObject[keys[j]] = values[i][j];
+    }
+    
+    newArray.push(newObject);
+  }
+  
+  return newArray
+};
