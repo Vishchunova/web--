da6ff247ca90091a2c4aeb18dc650a8c5c7aecdb
Vishchunova <92801027+Vishchunova@users.noreply.github.com>
2022-05-20 00:08:15 +0300

    Create script1.js

---
+++ задание 12/script1.js
@@ -0,0 +1,9 @@
+var groceries = ['чай', 'шпроты', 'печенье', 'сахар', 'чипсы'];
+var shoppingList = '';
+
+for (var i = 0; i < groceries.length; i++) {
+  if (i >= 1) {
+    shoppingList += ', ';
+  }
+  shoppingList += groceries[i];
+}
