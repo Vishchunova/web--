86ce1fb566bcca7d0d2f5902ff433eea7c7eecf2
Vishchunova <92801027+Vishchunova@users.noreply.github.com>
2022-05-20 10:37:53 +0300

    Update script4.js

--- задание 13/script4.js
+++ задание 13/script4.js
@@ -1,1 +1,23 @@
-
+function getPrice(time, isUrgent) {
+  var standardPrice = 1500;
+  var urgentPrice = 3750;
+  var discount = 250;
+  var standardPriceTime = 150;
+  
+  if (time < (standardPriceTime * 2) && isUrgent) {
+    discount = 0;
+  }
+  
+  if (isUrgent && time <= standardPriceTime) {
+    return (time / 2) * urgentPrice;
+  }  
+  else if (isUrgent && time > standardPriceTime) {
+    return (time / 2) * (urgentPrice - discount);
+  }  
+  else if (!isUrgent && time <= standardPriceTime) {
+    return time * standardPrice;
+  }  
+  else if (!isUrgent && time > standardPriceTime) {
+    return time * (standardPrice - discount);
+  }
+}
